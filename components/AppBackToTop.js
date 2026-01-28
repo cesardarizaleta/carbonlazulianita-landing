602669857b7c70e3f91898e9f16e94e241413e2a
cesardarizaleta <cesardarizaleta@gmail.com>
2026-01-28 10:27:04 -0400

    Add SEO metadata and Google Tag Manager integration to seo.txt

--- components/AppBackToTop.js
+++
@@ -1,9 +0,0 @@
-class AppBackToTop extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-      <a href="#" class="back-to-top"><i class="lni lni-chevron-up"></i></a>
-      `;
-    }
-}
-
-customElements.define('app-back-to-top', AppBackToTop);
